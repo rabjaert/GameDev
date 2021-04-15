e29f29a13fbe14445800bd07f37f5f8bbedb5bd5
Niklas <niklasberby@gmail.com>
2021-04-16 01:43:48 +0200

    sdl

--- game/main.cpp
+++ game/main.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <SDL.h>
 
-
-int main()
+int main(int argc, char **argv)
 {
+	if ((SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0)) {
+		puts("error");
+	}
 	printf("%s\n", "Hello world");
 	return 0;
 }
